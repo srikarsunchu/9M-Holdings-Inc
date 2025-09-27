a3938946db899781e316ac7e5e74ba93db1b2b86
Srikar Sunchu <srikarsunchu@gmail.com>
2025-09-27 13:12:37 +0530

    Use Resend sandbox domain

--- api/contact.js
+++ api/contact.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
 
     // Send email using Resend
     const { data, error } = await resend.emails.send({
-      from: 'Contact Form <noreply@9m.site>',
+      from: 'Contact Form <onboarding@resend.dev>',
       to: ['ops@9m.site'],
       subject: 'New Contact Form Submission',
       html: `
