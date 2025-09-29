2dbad65aaea3cfb5b3a0c46803bd044943148e35
Srikar Sunchu <srikarsunchu@gmail.com>
2025-09-29 15:43:25 +0530

    Fix Vercel serverless function format

--- api/contact.js
+++ api/contact.js
@@ -1,8 +1,8 @@
-const { Resend } = require('resend');
+import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   // Only allow POST requests
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
